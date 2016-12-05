b9552743d9383816b66610ec14c0b2f367481314
Edgar Pino <edgarbeltran@me.com>
2016-12-04 22:19:58 -0600

    added cond. example

--- exercises/modules/misc_utils.exs
+++ exercises/modules/misc_utils.exs
@@ -9,6 +9,20 @@ defmodule ModulePlayground.Misc.Util do
         f.(a)
     end
 
+    def day_abbreviation(day) do
+
+        cond do
+            day == :Monday -> "M"
+            day == :Tuesday -> "Tu"
+            day == :Wednesday -> "W"
+            day == :Thursday -> "Th"
+            day == :Friday -> "F"
+            day == :Saturday -> "Sa"
+            day == :Sunday -> "Su"
+        end
+        
+    end
+
 
 
 end
