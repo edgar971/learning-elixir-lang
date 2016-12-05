8e59f0d1f7df53966111317a2825ec0e84b7ec8b
Edgar Pino <edgarbeltran@me.com>
2016-12-04 22:39:06 -0600

    added case

--- exercises/modules/misc_utils.exs
+++ exercises/modules/misc_utils.exs
@@ -19,8 +19,23 @@ defmodule ModulePlayground.Misc.Util do
             day == :Friday -> "F"
             day == :Saturday -> "Sa"
             day == :Sunday -> "Su"
+
+            true -> "Invalid day"
         end
-        
+
+    end
+
+    def describe_date(date) do
+
+        case(date) do
+            {1, _, _} -> "Brand new month!"
+            {25, 12, _} -> "Merry Christmas"
+            {25, month, _} -> "Only #{12 - month} months left."
+            # Pattern matching
+            {_, month,_} when month <= 12 -> "Just an average day."
+            {_,_,_} -> "Invalid month"
+        end
+
     end
 
 
