eae913ef495846b823b7b5be230da47043e56357
Edgar Pino <edgarbeltran@me.com>
2016-12-13 22:39:36 -0600

    added unit test

--- projects/tweet_app/lib/tweet_app/file_reader.ex
+++ projects/tweet_app/lib/tweet_app/file_reader.ex
@@ -6,10 +6,17 @@ defmodule TweetApp.FileReader do
     # 4. Make sure it's a valid tweet
     def get_msgs_to_tweet(path) do 
         File.read!(path)
+        |> pick_string
+    end
+
+    def pick_string(str) do 
+    
+        str 
         |> String.split("\n")
         |> Enum.map(&String.trim/1)
         |> Enum.filter(&String.length(&1) <= 140)
         |> Enum.random()
+
     end
 
 end
---
+++ projects/tweet_app/test/tweet_app/file_reader_test.exs
@@ -0,0 +1,33 @@
+defmodule FileReaderTest do 
+
+    use ExUnit.Case
+
+    test "Passing a file should return a string" do 
+
+        str = TweetApp.FileReader.get_msgs_to_tweet(
+            Path.join("#{:code.priv_dir(:tweet_app)}", "tweets.txt")
+        )
+
+        assert str != nil
+        
+    end
+
+    test "will not return a string longer than 140 characters" do 
+
+        str = TweetApp.FileReader.get_msgs_to_tweet(
+            Path.join("#{:code.priv_dir(:tweet_app)}", "/tests/too_long.txt")
+        )
+
+        assert str == "short line"
+
+    end
+
+    test "An empty string should return an empty string" do 
+
+        str = TweetApp.FileReader.pick_string("")
+
+        assert str == ""
+
+    end
+
+end
